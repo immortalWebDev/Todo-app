96c45696a148c4298d62c2bce8db0cc574cf4a5c
immortalWebDev <pgbadgujar007@gmail.com>
2024-08-23 00:14:51 +0530

    Added basic footer

---
+++ my-todo/components/Footer.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>Created by Piyush Badgujar</p>
+    </footer>
+  );
+}
+
+export default Footer;
